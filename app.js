176d899191b567fac66b7b030c36892437ee504c
Pooja <poojasihag.dev@gmail.com>
2024-10-01 17:46:58 +0530

    added separation of concerns in models and created owner model

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const db = require("./config/mongoose-connection");
+const owenersRouter = require("./routes/owenersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.use("/owners", owenersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
